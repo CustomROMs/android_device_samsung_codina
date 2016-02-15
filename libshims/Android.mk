9315feb9874e4d1d070be533bb5a0c13da35f53c
Ivan Grinko <iivanich@gmail.com>
2016-02-15 12:47:35 +0200

    pyramid: Properly shim VectorImpl
    VectorImpl needs to have virtual methods added to the object itself.
    You cannot do this by simply adding the symbols, the object itself
    needs to be created with the correct sized virtual function table.
    
    Create a separate lib that does this shimming because we need to
    add to each shared library which makes use of the VectorImpl symbols
    to make sure the libcutils versions aren't used.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -5,10 +5,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-     VectorImpl.c
+	SharedBuffer.cpp \
+	VectorImpl.cpp
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
-LOCAL_MODULE := libshim_audio
+LOCAL_C_INCLUDES += external/safe-iop/include
+LOCAL_SHARED_LIBRARIES := liblog libutils
+LOCAL_MODULE := libLog
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
