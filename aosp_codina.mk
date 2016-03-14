c3a9a0eaa967f5e7cfcb29dc1cc9ccf85f5fc561
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-14 20:13:20 +0700

    Inherit FML product
    Change-Id: I9c2c396c92490894ec992d7f523bef77ad209ce1

--- aosp_codina.mk
+++ aosp_codina.mk
@@ -1,5 +1,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit FML configuration
+$(call inherit-product, vendor/fml/config/common.mk)
+
 $(call inherit-product, device/samsung/codina/codina.mk)
 
 # Discard inherited values and use our own instead.
