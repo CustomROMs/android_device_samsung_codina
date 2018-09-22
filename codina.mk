a7480ccb71fbff7ebb42dcce4fedbe2914a4f8ab
Shilin Victor <chrono.monochrome@gmail.com>
2018-09-22 18:26:43 +0300

    Switch to LineageOS legacy camera HIDL
    Change-Id: Id51a409f36888380881e39a70f68d9bff03c1bc7

--- codina.mk
+++ codina.mk
@@ -271,14 +271,14 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
 
 # Camera legacy
-#PRODUCT_PACKAGES += \
-#    android.hardware.camera.provider@2.4-impl-legacy \
-#    camera.device@1.0-impl-legacy
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy
 
 # Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+#PRODUCT_PACKAGES += \
+#    android.hardware.camera.provider@2.4-impl \
+#    camera.device@1.0-impl
 
 # health
 PRODUCT_PACKAGES += \
