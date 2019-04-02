a2236a9a7fc0317ea30d53ad109303b2528335ed
Shilin Victor <radicaldreamer00001@gmail.com>
2019-04-02 16:46:59 +0300

    codina: add libsymbols

---
+++ libsymbols/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	parcel.cpp
+
+LOCAL_SHARED_LIBRARIES := libbinder
+LOCAL_MODULE := libsymbols
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ libsymbols/parcel.cpp
@@ -0,0 +1,10 @@
+#include <sys/cdefs.h>
+
+extern "C" {
+
+__strong_alias(
+    _ZN7android6Parcel13writeString16EPKtj,
+    _ZN7android6Parcel13writeString16EPKDsj
+);
+
+}
