39a2fa17fa15077c08df54bb74f050b61d2aa995
Shilin Victor <radicaldreamer00001@gmail.com>
2019-06-01 21:02:28 +0300

    codina: kang libdmitry from manta

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-# camera
+# audio libs
 
 include $(CLEAR_VARS)
 
@@ -11,6 +11,19 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES += external/safe-iop/include
 LOCAL_SHARED_LIBRARIES := liblog libutils
 LOCAL_MODULE := libLog
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+#
+# compat symbols for gps
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := libshim_gps.c
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
+LOCAL_MODULE := libshim_gps
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
