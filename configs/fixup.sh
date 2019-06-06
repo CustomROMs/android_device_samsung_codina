57892d7d832a5136f1109124d4c9b372a8ce9227
Shilin Victor <radicaldreamer00001@gmail.com>
2019-06-06 12:29:42 +0300

    codina: unfuck kernel flashing

--- configs/fixup.sh
+++ configs/fixup.sh
@@ -1,14 +1,9 @@
 #!/sbin/sh
 
 if grep -q janice /default.prop || grep -iq i9070 /default.prop ; then
+flash_image /dev/block/mmcblk0p15 /tmp/install/janice/boot.img
 cp -r /tmp/janice/* /system/
-#sed -i "s,8160,9070,g" /tmp/build.prop
-#sed -i "s,codina,janice,g" /tmp/build.prop
-#cp /tmp/build.prop /system/
-dd if=/tmp/install/janice/boot.img of=/dev/block/mmcblk0p15 bs=1M
-sync
 elif grep -q codina /default.prop || grep -iq i8160 /default.prop ; then
+flash_image /dev/block/mmcblk0p15 /tmp/install/codina/boot.img
 cp -r /tmp/codina/* /system/
-dd if=/tmp/install/codina/boot.img of=/dev/block/mmcblk0p15 bs=1M
-sync
 fi
